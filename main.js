4d7ad4b931d7eacc82a28a687bb433c9cc83922b
“gonzagaa” <iigust4vo@gmail.com>
2023-09-19 14:43:06 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+function onScroll () {
+
+    if(scrollY > 0) {
+        document.querySelector("#navigation").classList.add("scroll")
+    } else {
+        document.querySelector("#navigation").classList.remove("scroll")
+    }
+
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+    origin: 'top',
+    distance: '100px',
+    duration: '700'
+}).reveal(`.home-image`);
+
+ScrollReveal({
+    origin: 'top',
+    distance: '100px',
+    duration: '2000'
+}).reveal(`.input`);
+
+/*Depositions Caroussel slider swiper */
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+});
