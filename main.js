23c00cbb9fbec0533e46b812619fcf444afbe93a
“gonzagaa” <iigust4vo@gmail.com>
2023-09-27 16:10:01 -0300

    added button ig

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-/*menu on scroll*/
-function onScroll () {
+window.addEventListener('scroll', onScroll)
 
+onScroll()
+function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
     if(scrollY > 0) {
         document.querySelector("#navigation").classList.add("scroll")
     } else {
         document.querySelector("#navigation").classList.remove("scroll")
     }
+}
 
+function showButtonInstagramOnScroll() {
+    if(scrollY > 500) {
+        document.querySelector("#buttonInstagram").classList.add("show")
+    } else {
+        document.querySelector("#buttonInstagram").classList.remove("show")
+    }
 }
 
 /*button close and open menu*/
