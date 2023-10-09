ac4ce19484931dd34fba7b9a48180a13fdb367d9
“gonzagaa” <iigust4vo@gmail.com>
2023-10-09 09:47:19 -0300

    update items

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -73,33 +73,4 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
-  var swiper = new Swiper(".mySwiperShorts", {
-    slidesPerView: 1,
-    spaceBetween: 20,
-
-    loop: true,
-    sticky: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        loop: true,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-        loop: true,
-      },
-
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 50,
-        loop: true,
-      }
-    },
-  });
 
